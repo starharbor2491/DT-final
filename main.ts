8a5327d4a0b7a8f65ab77407b92d9c495b967752
starharbor2491 <koioshong@hotmail.com>
2026-09-21 15:17:39 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,9 @@
-input.onButtonPressed(Button.A, function () {
-    x += 5
-})
-input.onButtonPressed(Button.B, function () {
-    x += -5
-})
-let x = 0
 hummingbird.startHummingbird()
-if (10 < hummingbird.getSensor(SensorType.Distance, ThreePort.One)) {
-    hummingbird.setPositionServo(FourPort.One, 90)
-} else {
-    hummingbird.setPositionServo(FourPort.One, 0)
-}
+hummingbird.setPositionServo(FourPort.One, 50)
 basic.forever(function () {
-    hummingbird.setPositionServo(FourPort.One, x)
-    basic.showString("" + (x))
+    if (10 < hummingbird.getSensor(SensorType.Distance, ThreePort.One)) {
+        hummingbird.setPositionServo(FourPort.One, 90)
+    } else {
+        hummingbird.setPositionServo(FourPort.One, 20)
+    }
 })
