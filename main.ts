4c5fd94d91d83f8bc9385849d424e64fa297baeb
starharbor2491 <koioshong@hotmail.com>
2026-09-22 09:53:27 +0800

    added second imput

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ hummingbird.startHummingbird()
 hummingbird.setPositionServo(FourPort.One, 50)
 basic.forever(function () {
     if (50 < hummingbird.getSensor(SensorType.Distance, ThreePort.One)) {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
         hummingbird.setPositionServo(FourPort.One, 140)
     } else {
         hummingbird.setPositionServo(FourPort.One, 95)
