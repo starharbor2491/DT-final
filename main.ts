b5f8ae41afc8ee01d11e13cd33804a6f0147138d
starharbor2491 <koioshong@hotmail.com>
2026-09-22 10:24:15 +0800

    final?

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
+let x = 0
 hummingbird.startHummingbird()
 hummingbird.setPositionServo(FourPort.One, 115)
 basic.forever(function () {
     if (5 > hummingbird.getSensor(SensorType.Distance, ThreePort.One)) {
+        x = 0
         hummingbird.setPositionServo(FourPort.One, 115)
     } else {
+        x = 1
         hummingbird.setPositionServo(FourPort.One, 140)
     }
 })
 basic.forever(function () {
-    basic.showString("" + (hummingbird.getSensor(SensorType.Distance, ThreePort.One)))
-    if (input.buttonIsPressed(Button.A)) {
-        hummingbird.setPositionServo(FourPort.One, 115)
-    } else {
-        hummingbird.setPositionServo(FourPort.One, 140)
+    if (x == 0) {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
     }
 })
