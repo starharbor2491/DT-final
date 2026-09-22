3d81618de40e52b8ff9df623f4945fd8a9747ade
starharbor2491 <koioshong@hotmail.com>
2026-09-22 10:12:01 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,17 @@
 hummingbird.startHummingbird()
-hummingbird.setPositionServo(FourPort.One, 50)
+hummingbird.setPositionServo(FourPort.One, 115)
 basic.forever(function () {
-    if (50 < hummingbird.getSensor(SensorType.Distance, ThreePort.One)) {
-        music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
-        hummingbird.setPositionServo(FourPort.One, 140)
+    if (5 > hummingbird.getSensor(SensorType.Distance, ThreePort.One)) {
+        hummingbird.setPositionServo(FourPort.One, 115)
     } else {
-        hummingbird.setPositionServo(FourPort.One, 95)
+        hummingbird.setPositionServo(FourPort.One, 140)
+    }
+})
+basic.forever(function () {
+    basic.showString("" + (hummingbird.getSensor(SensorType.Distance, ThreePort.One)))
+    if (input.buttonIsPressed(Button.A)) {
+        hummingbird.setPositionServo(FourPort.One, 115)
+    } else {
+        hummingbird.setPositionServo(FourPort.One, 140)
     }
 })
